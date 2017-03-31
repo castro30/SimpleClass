daf6b5abc22c4c8db53df3872e8f33e56dd33f9e
castro30 <castro30@yahoo.com>
2017-03-31 16:41:39 -0500

    Added two more assignments

---
+++ SimpleClassDemo5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Messenger
+{
+    class MessengerDemo
+    {
+        public string message = "Default Message ";
+        public void DisplayMessage()
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void Main()
+        {
+            MessengerDemo objMessage = new MessengerDemo();
+            objMessage.message = "Hello from C#";
+            objMessage.DisplayMessage();
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SimpleMethodCall.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace SimpleMethodCall
+{
+    // Summary
+    // Class with data members x and y, specifying coordinates
+    //Summary
+    class Point
+    {
+        //data members
+        private int xValue;
+        private int yValue;
+
+        // summary
+        // Class SimpleMethodMain which uses the class Point
+        // summary
+
+        public void Accept()
+        {
+            Console.WriteLine("Enter the coordinates - ");
+            xValue = Convert.ToInt32(Console.ReadLine());
+            yValue = Convert.ToInt32(Console.ReadLine());
+            return;
+        }
+
+        ///<summary>
+        ///Method to display data members
+        ///</summary>
+
+        public void Display()
+        {
+            Console.WriteLine("The coordinates are  - ");
+            Console.WriteLine("{0},{1}", xValue, yValue);
+            return;
+        }
+    }
+
+    public class SimpleMethod
+    {
+        // summary
+        // The main entry point for the application.
+        // summary
+        public static void Main()
+        {
+
+
+            Point myPoint = new Point();
+            myPoint.Accept();
+            myPoint.Display();
+
+            /* Point myPoint = new Point();
+             Console.WriteLine("The coordinates are  - ");
+             myPoint.xValue = 10;
+             myPoint.yValue = 15;
+             Console.WriteLine("{0},{1}", myPoint.xValue, myPoint.yValue);
+             Console.ReadLine();
+             return; */
+
+            Console.ReadKey();
+        }
+    }
+}
